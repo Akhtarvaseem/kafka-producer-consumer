a06cd98c9effbb6d3ee9663b6337ad4c571b7b44
Akhtarvaseem <99640371+Akhtarvaseem@users.noreply.github.com>
2024-07-15 18:06:40 +0530

    Delete microservices-hotel directory

--- microservices-hotel/src/main/java/com/micro/hotelservices/customException/IdNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.micro.hotelservices.customException;
-
-@SuppressWarnings("serial")
-public class IdNotFoundException extends RuntimeException{
-
-	public IdNotFoundException(String msg) {
-		super(msg);
-	}
-}
